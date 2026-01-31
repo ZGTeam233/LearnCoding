6f4e91d0de53b5f6e62eff082cee90c63062fb42
Zilin Zheng <wsnzg6@outlook.com>
2026-01-31 13:52:33 +0800

    -

--- ActivityTest/app/src/main/java/com/github/zgteam233/activitytest/FirstActivity.kt
+++ ActivityTest/app/src/main/java/com/github/zgteam233/activitytest/FirstActivity.kt
@@ -1,6 +1,10 @@
 package com.github.zgteam233.activitytest
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +14,22 @@ class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_layout)
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            Toast.makeText(this, "你点了按钮1", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "你点了添加", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "你点了移除", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
